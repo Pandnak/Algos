127b625616890b09acbd7806bf9a47b5e9579210
Andrew <82667860+Pandnak@users.noreply.github.com>
2024-03-07 17:12:40 +0300

    home task fixed.

--- sem_1_arrays/zeros_last.py
+++ sem_1_arrays/zeros_last.py
@@ -1,8 +1,13 @@
 def zeros_to_end(arr):
     i = 0
     j = len(arr) - 1
-    while (i <= j):
-        if (arr[i] == 0):
+    while (i < j):
+        if (arr[i] == 0 and arr[j] != 0):
             arr[i], arr[j] = arr[j], arr[i]
+            i += 1
             j -= 1
-        i += 1
+        elif (arr[i] == 0):
+            j -= 1
+        else:
+            i += 1
+    return arr
